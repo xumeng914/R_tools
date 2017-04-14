8a9c380b313729a27472d1f65ab0319474b740d1
xumeng914 <xumeng320@sina.com>
2017-04-14 10:36:40 +0800

    stl分解

--- time_series.R
+++ time_series.R
@@ -15,3 +15,25 @@ tsales.subset <- window(tsales, start=c(2003, 5), end=c(2004, 6))
 tsales.subset
 
 
+#########################################
+# ma 移动平均
+library(forecast)
+opar <- par(no.readonly=TRUE)
+par(mfrow=c(2,2))
+ylim <- c(min(Nile), max(Nile))
+plot(Nile, main="Raw time series")
+plot(ma(Nile, 3), main="Simple Moving Averages (k=3)", ylim=ylim)
+plot(ma(Nile, 7), main="Simple Moving Averages (k=7)", ylim=ylim)
+plot(ma(Nile, 15), main="Simple Moving Averages (k=15)", ylim=ylim)
+par(opar)
+
+###########################################
+## 分解
+plot(AirPassengers)
+lAirPassengers <- log(AirPassengers) ## 对数化 相乘模型
+plot(lAirPassengers, ylab="log(AirPassengers)")
+fit <- stl(lAirPassengers, s.window="period")  # s.windows="periodic"可使得季节效应在各年间都一样
+plot(fit)
+fit$time.series
+
+
