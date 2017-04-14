d6601d460caf40af92702475ebdb37d629d09467
xumeng914 <xumeng320@sina.com>
2017-04-14 09:50:03 +0800

    1

---
+++ time_series.R
@@ -0,0 +1,8 @@
+sales <- c(18, 33, 41, 7, 34, 35, 24, 25, 24, 21, 25, 20,
+           22, 31, 40, 29, 25, 21, 22, 54, 31, 25, 26, 35)
+
+tsales <- ts(sales, start=c(2003, 1), frequency=12)
+tsales
+
+
+plot(tsales)
