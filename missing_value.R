2e42daae3fdc45fa78e3a4d5ffa0e1b86f667632
xumeng914 <xumeng320@sina.com>
2017-07-25 15:52:01 +0800

    3

--- missing_value.R
+++ missing_value.R
@@ -22,6 +22,38 @@ library("VIM")
 aggr(sleep, prop=FALSE, numbers=TRUE)
 aggr(sleep, prop=TRUE, numbers=TRUE)
 
-
-
 matrixplot(sleep) #红色是缺失值，浅色值小，黑色值大
+
+
+#相关性探索缺失值
+
+x <- as.data.frame(abs(is.na(sleep)))
+head(sleep, n=5)
+head(x, n=5)
+
+y <- x[which(apply(x,2,sum)>0)]
+cor(y)
+cor(sleep, y, use="pairwise.complete.obs")
+
+
+##理性处理不完整数据
+
+# mydata中所有包含缺失数据的行都被删除，然后结果才存储到newdata中
+newdata <- na.omit(mydata)
+cor(na.omit(sleep))
+fit <- lm(Dream ~ Span + Gest, data=na.omit(sleep))
+summary(fit)
+
+
+###多重插补
+library(mice)
+data(sleep, package="VIM")
+imp <- mice(sleep, seed=123)
+fit <- with(imp, lm(Dream ~ Span + Gest))
+pooled <- pool(fit)
+summary(pooled)
+
+
+
+
+
