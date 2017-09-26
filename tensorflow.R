c496f1906f5101becfc63e510cbe0b26611ce404
xumeng914 <xumeng320@sina.com>
2017-09-26 14:35:43 +0800

    2

--- tensorflow.R
+++ tensorflow.R
@@ -1,2 +1,7 @@
 install.packages("tensorflow")   #安装tensorflow，当然也可以用Rstudio手动安装
 library(tensorflow)  #加载包
+
+m1 <- matrix(c(1.0, 2.0, 
+               3.0, 4.0), byrow=T, nrow=2)
+m3 <- tf$constant(matrix(c(1.0, 2.0, 
+                           3.0, 4.0), byrow=T, nrow=2))
